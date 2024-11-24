a1bb1d1daad6410211a47b66a83e9551522eabfb
Jonathan Borja <jborja.one@gmail.com>
2024-11-24 12:22:59 -0700

    done

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace APIsAndJSON
+using System.Net.Http;
+using Newtonsoft.Json.Linq;namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private HttpClient _client;
+
+        public RonVSKanyeAPI(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()        {
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();            
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);            
+            return ronQuote[0].ToString();
+        }
     }
 }
